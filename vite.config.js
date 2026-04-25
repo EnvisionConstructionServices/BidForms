fada0e40d71e7f4815816f7308385e4f994a4054
EnvisionCS <emerson@envision-cs.com>
2026-04-24 20:23:15 -0400

    Add Vite configuration for React project

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // This MUST match your repository name exactly (case-sensitive)
+  base: '/BidForms/', 
+})
